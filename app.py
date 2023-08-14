6753c4782e884faa5f54162ba182640fac30e523
fearlessserver <senatoreniola@gmail.com>
2023-08-14 02:21:09 +0100

    adding (inheritance)

--- app.py
+++ app.py
@@ -357,13 +357,33 @@
 
 # constructors
 # exercise
-class Person:
-    def __init__(self, name):
-        self.name = name
+# class Person:
+#     def __init__(self, name):
+#         self.name = name
 
-    def talk(self):
-        print(f"Hi i am ")
+#     def talk(self):
+#         print(f"Hi i am {self.name}")
 
-sen = Person("senator olo")
-print(sen.name)
-sen.talk()
+# sen = Person("senator olorunnisola")
+# sen.talk()
+
+# bob = Person("bob sen")
+# bob.talk()
+
+# inheretance
+# class Mammal:
+#      def walk(self):
+#         print("walk")
+# class Dog(Mammal):
+#    def bark(self):
+#        print("bark")
+
+# class Cat(Mammal):
+#     def be_annoying(self):
+#         print("annoying")
+
+# dog1 = Dog()
+# dog1.walk
+
+# cat1 = Cat()
+# cat1.be_annoying
